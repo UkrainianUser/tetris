e1f132c1d1dd86e2df734a360c5b1dbbda38b9cd
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2024-02-20 17:44:34 +0200

    add tetromino functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const PLAYEFIELD_COLUMNS = 10;
 const PLAYEFIELD_ROWS = 20;
+const TETROMINO_NAMES = ["O"];
+
+const TETROMINOES = {
+	O: [
+		[1, 1],
+		[1, 1],
+	],
+};
+
+function convertPositionToIndex(row, column) {
+	return row * PLAYEFIELD_COLUMNS + column;
+}
 
 let playfield;
+let tetromino;
 
 function generatePlayField() {
 	for (let i = 0; i < PLAYEFIELD_ROWS * PLAYEFIELD_COLUMNS; i++) {
@@ -9,4 +22,42 @@ function generatePlayField() {
 		document.querySelector(".grid").append(divEl);
 	}
 }
+
+function generateTetromino() {
+	const name = TETROMINO_NAMES[0];
+	const matrix = TETROMINOES[name];
+
+	tetromino = {
+		name,
+		matrix,
+		row: 3,
+		column: 5,
+	};
+}
+
 generatePlayField();
+generateTetromino();
+
+const cells = document.querySelectorAll(".grid div");
+
+function drawPlayField() {
+	// cells[15].classList.add("O");
+}
+
+function drawTetromino() {
+	const name = tetromino.name;
+	const tetrominoMatrixSize = tetromino.matrix.length;
+
+	for (let row = 0; row < tetrominoMatrixSize; row++) {
+		for (let column = 0; column < tetrominoMatrixSize; column++) {
+			const cellIndex = convertPositionToIndex(
+				tetromino.row + row,
+				tetromino.column + column
+			);
+			cells[cellIndex].classList.add("O");
+		}
+	}
+}
+
+drawTetromino();
+drawPlayField();
